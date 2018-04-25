26feba03c2749aa1bd20b67af2d6b7924699a962
ポール ウェッブ <netopwibby@thenetwork.email>
2018-04-25 09:41:35 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# hype-title
+
+[![N](https://img.shields.io/badge/%F0%9F%91%8D%F0%9F%8F%BE-NetOperatorWibby/hype--title-07d0eb.svg?style=flat-square)](https://git.inc.sh/NetOperatorWibby/hype-title)
+
+> Set the title of your tab to the folder you are in
+
+
+
+### Install
+
+Automatic: `npm i hype-title`
+Manual: Add `hype-title` to the plugins list in your `~/.hyper.js` config file
---
+++ index.js
@@ -0,0 +1,90 @@
+"use strict";
+
+
+
+//	P A C K A G E
+
+const { exec } = require("child_process");
+
+
+
+//	P R O G R A M
+
+const sessions = {};
+
+const debounce = (func, wait, immediate) => {
+	let timeout;
+
+	return function () {
+		const context = this;
+		const args = arguments;
+
+		const later = function () {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+
+		const callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+
+		if (callNow) func.apply(context, args);
+	};
+};
+
+const setTitle = (pid, uid) => {
+  exec(`lsof -p ${pid} | grep cwd | tr -s " " | cut -d " " -f9-`, (err, cwd) => {
+    if (err) {
+      console.error(err);
+    } else {
+      cwd = cwd.split("/").pop().replace("\n", "");
+
+      if (cwd === process.env.USER) cwd = "~";
+      else if (cwd === "") cwd = "/";
+
+      store.dispatch({
+        type: "SESSION_SET_XTERM_TITLE",
+        title: cwd,
+        uid
+      });
+    }
+  });
+};
+
+const debouncedTitle = debounce(setTitle, 10); // 250
+
+
+
+//	E X P O R T
+
+exports.middleware = (store) => (next) => (action) => {
+  switch (action.type) {
+    case "SESSION_ADD":
+      sessions[action.uid] = {
+        pid: action.pid
+      };
+
+      break;
+
+    case "SESSION_PTY_EXIT":
+    case "SESSION_USER_EXIT":
+      delete sessions[action.uid];
+      break;
+
+    case "SESSION_PTY_DATA": {
+      let session = sessions[action.uid];
+      debouncedTitle(session.pid, action.uid);
+
+      break;
+    }
+
+		case "SESSION_SET_PROCESS_TITLE": {
+      const session = sessions[action.uid];
+      debouncedTitle(session.pid, action.uid);
+
+      break;
+    }
+  }
+
+  next(action);
+};
